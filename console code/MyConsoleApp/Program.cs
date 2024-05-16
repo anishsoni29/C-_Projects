5a7dbccb5c0debb36df088bebca615d6106c7612
Anish Soni <anish.21cse189@rtu.ac.in>
2024-05-16 15:35:48 +0400

    added practise projects

---
+++ console code/MyConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next();
+int roll3 = dice.Next();
+
+Console.WriteLine($"First roll" : {roll1});
+Console.WriteLine($"Second roll" : {roll2});
+Console.WriteLine($"Third roll" : {roll3});
+
---
+++ object_understanding/object/Program.cs
@@ -0,0 +1,13 @@
+﻿Random dice = new Random();
+
+int roll1 = dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+int roll3 = dice.Next(1,7);
+
+int total = roll1 + roll2 +roll3
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3} \n");
+
+Console.WriteLine($"Total :{total}");
