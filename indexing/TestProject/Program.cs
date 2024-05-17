82061048b84edcd1de3e669eb45fc987bb03f5ec
Anish Soni <anish.21cse189@rtu.ac.in>
2024-05-17 15:26:29 +0400

    used the indexOf method

---
+++ indexing/TestProject/Program.cs
@@ -0,0 +1,9 @@
+﻿string message = "My name is Anish.";
+
+int openPosition = message.IndexOf('M');
+int closePosition = message.IndexOf('.');
+
+Console.WriteLine(openPosition);
+Console.WriteLine(closePosition);
+
+Console.WriteLine(openPosition - closePosition);
