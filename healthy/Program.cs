fc749b15a64c8fb555a1b6a6ae58f2ebec9902ff
Anish Soni <anish.21cse189@rtu.ac.in>
2024-05-18 12:25:44 +0400

    checked whether the persion is healthy or unhealthy

--- healthy/Program.cs
+++ healthy/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string status = "Healthy";
+
+Console.WriteLine($"Start: {status}");
+SetHealth(status, false);
+Console.WriteLine($"End:{status}");
+
+void SetHealth(string status, bool isHealthy)
+{
+    status = (isHealthy ? "Healthy" : "Unhealthy");
+    Console.WriteLine($"Middle : {status}");
+}
