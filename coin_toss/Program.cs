4a4d38d10cf98dc294e691bf9c8ab54cb6b48889
Anish Soni <anish.21cse189@rtu.ac.in>
2024-05-20 09:57:21 +0400

    intiated with the coin code

---
+++ coin_toss/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int target = 60;
+int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+int[] result = TwoCoins(coins, target);
+
+if (result.Length == 0)
+{
+    Console.WriteLine("No two coins make change");
+}
+else
+{
+    Console.WriteLine($"Change found at position {result[0]} and {result[1]}");
+}
+
+
+int[] TwoCoins(int[] coins, int target)
+{
+    for (int curr = 0; curr < coins.Length; curr++)
+    {
+        for (int next = curr + 1; next < coins.Length; next++)
+        {
+            if (coins[curr] + coins[next] == target)
+            {
+                return new int[] { curr, next };
+            }
+        }
+
+    }
+    return new int[0];
+}
